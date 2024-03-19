2e0731b53e774b3a5e5eb28f7d30d2c39d514909
Tribeoftech <gratefulgrowing333@gmail.com>
2024-03-19 16:15:01 -0500

    editing imports adding back new dependcies and fixing files manager fingers crossed

--- server.js
+++ server.js
@@ -1,28 +1,14 @@
-/**
- * Sets up an Express server instance with JSON body parsing middleware and the
- * application routes. Exports the Express app instance.
- */
-// Setting up the Express Server
+import { env } from 'process';
+import express from 'express';
+import routingController from './routes/index';
 
-import express from "express";
-import routes from "./routes/index";
+const PORT = env.PORT || 5000;
 
 const app = express();
-const port = process.env.PORT || 5000;
 
-/**
- * Sets up middleware and routes in the Express app instance.
- *
- * Uses express.json() middleware to parse JSON request bodies.
- * Registers the routes defined in ./routes/index.js.
- * Starts listening on the defined port.
- * Exports the app instance.
- */
 app.use(express.json());
-app.use(routes);
+routingController(app);
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
-
-module.exports = app;
--- worker.js
+++ worker.js
@@ -1,55 +1,45 @@
-/**
- * Processes image files from a queue by generating thumbnails.
- * Creates 3 thumbnail sizes for each image and saves them to the file system.
- * Also processes a user queue by looking up user info.
- */
-import Queue from "bull";
-import { writeFileSync } from "fs";
-import { ObjectId } from "mongodb";
-import dbClient from "./utils/db";
+import DBClient from './utils/db';
 
-const imageThumbnail = require("image-thumbnail");
+const Bull = require('bull');
+const { ObjectId } = require('mongodb');
+const imageThumbnail = require('image-thumbnail');
+const fs = require('fs');
 
-const fileQueue = new Queue("file processing");
-const userQueue = new Queue("userQ");
+const fileQueue = new Bull('fileQueue');
+const userQueue = new Bull('userQueue');
 
-// Function creates a thumbnail based on passed in file and width
-const createThumb = async (path, options) => {
+const createImageThumbnail = async (path, options) => {
   try {
     const thumbnail = await imageThumbnail(path, options);
-    const thumbPath = `${path}_${options.width}`;
-    // console.log('Thumbnail path is:', thumbPath);
-    writeFileSync(thumbPath, thumbnail);
-  } catch (err) {
-    console.log(err);
+    const pathNail = `${path}_${options.width}`;
+
+    await fs.writeFileSync(pathNail, thumbnail);
+  } catch (error) {
+    console.log(error);
   }
 };
 
-// Calls on createThumb to create 3 files of different
-// image widths
 fileQueue.process(async (job) => {
-  const { fileId, userId } = job.data;
-  if (!fileId) throw new Error("Missing fileId");
-  if (!userId) throw new Error("Missing userId");
+  const { fileId } = job.data;
+  if (!fileId) throw Error('Missing fileId');
 
-  const file = await dbClient.db.collection("files").findOne({
-    _id: new ObjectId(job.data.fileId),
-    userId: new ObjectId(job.data.userId),
-  });
-  if (!file) throw new Error("File not found");
+  const { userId } = job.data;
+  if (!userId) throw Error('Missing userId');
 
-  createThumb(file.localPath, { width: 500 });
-  createThumb(file.localPath, { width: 250 });
-  createThumb(file.localPath, { width: 100 });
-  console.log("Thumbnail processing complete");
+  const fileDocument = await DBClient.db.collection('files').findOne({ _id: ObjectId(fileId), userId: ObjectId(userId) });
+  if (!fileDocument) throw Error('File not found');
+
+  createImageThumbnail(fileDocument.localPath, { width: 500 });
+  createImageThumbnail(fileDocument.localPath, { width: 250 });
+  createImageThumbnail(fileDocument.localPath, { width: 100 });
 });
 
 userQueue.process(async (job) => {
   const { userId } = job.data;
-  if (!userId) throw new Error("Missing userId");
+  if (!userId) throw Error('Missing userId');
 
-  const user = dbClient.users.findOne({ _id: ObjectId(userId) });
-  if (!user) throw new Error("User not found");
+  const userDocument = await DBClient.db.collection('users').findOne({ _id: ObjectId(userId) });
+  if (!userDocument) throw Error('User not found');
 
-  console.log(`Welcome ${user.email}`);
+  console.log(`Welcome ${userDocument.email}`);
 });
